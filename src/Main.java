512822b613a948b1bfd447909693b9c4d68b55f8
gaoweipo <gaoweipo@hotmail.com>
2023-02-20 14:18:39 -0500

    Entrez un programme, afin d'afficher les caractères d'échappement

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        char c1 = '\\';
+        char c2 = '\'';
+        char c3 = '\"';
+        char c4 = '\u2605';
+        char c5 = '\101';
+        char c6 = '\t';
+        char c7 = '\n';
+
+    }
+}
